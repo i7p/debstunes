d344f2000e37674c5cdb10ef762eba0d0308c430
indian7p <48807646+indian7p@users.noreply.github.com>
2024-12-02 17:30:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const dataset = [
+    "confusion.mp3",
+    "continuous.mp3",
+    "derivative.mp3",
+    "discrete.mp3",
+    "distributions.mp3",
+    "expectation.mp3",
+    "fancy.mp3",
+    "i-dont-know.mp3",
+    "inteegral.mp3",
+    "knock-knock-yes-please.mp3",
+    "left-as-an-exercise.mp3",
+    "lets-talk-outside.mp3",
+    "lets-try-to-settle-down.mp3",
+    "no.mp3",
+    "omega.mp3",
+    "probability.mp3",
+    "probility.mp3",
+    "random-variable.mp3",
+    "right.mp3",
+    "sigma-algebra.mp3",
+    "some-people-are-just-out-of-their-minds.mp3",
+    "we-dont-really-care.mp3",
+    "we-wont-be-much-concerned.mp3",
+    "write-to-me.mp3",
+    "yes-please.mp3"
+];
+
+function pc(n){
+    const audio = document.createElement('audio');
+    audio.src = `phrases/${dataset[n]}`;
+    audio.controls = false;
+    const playerContainer = document.getElementById('player');
+    playerContainer.innerHTML = '';
+    playerContainer.appendChild(audio);
+    audio.play()
+}
+function surpriseMe(){
+    let n = Math.floor(Math.random()*dataset.length)
+    pc(n)
+}
